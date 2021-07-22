dce072a1986fd9e8aa4f2331935302a1ed37a33e
Jack Begley <shtekentoker@gmail.com>
2021-07-22 14:51:07 -0400

    Added Structure Locator

---
+++ src/main/java/com/endremastered/endrem/util/StructureLocator.java
@@ -0,0 +1,29 @@
+package com.endremastered.endrem.util;
+
+import com.endremastered.endrem.EndRemastered;
+import net.fabricmc.loader.FabricLoader;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.feature.StructureFeature;
+import org.spongepowered.asm.mixin.transformer.Config;
+
+
+public class StructureLocator {
+    public static StructureFeature<?> getStructureToLocate(String structureID) {
+      //  Config.setup(FMLPaths.CONFIGDIR.get().resolve(EndRemastered.MOD_ID + ".toml")); //dafuck
+        String mod_id = structureID.split(":")[0];
+
+        // Checks if mod is loaded
+        if (FabricLoader.INSTANCE.isModLoaded(mod_id)) {
+            Identifier resourceLocation = new Identifier(structureID);
+
+            // Checks if structure exists
+            if (Registry.STRUCTURE_FEATURE.containsId(resourceLocation)) {
+                return Registry.STRUCTURE_FEATURE.get(resourceLocation);
+            }
+        }
+
+        // Returns null if structure is invalid
+        return null;
+    }
+}
