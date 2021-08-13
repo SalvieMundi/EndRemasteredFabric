0eb185a88590d761cb52f48a2ea04d778e9d7b97
Jack Begley <shtekentoker@gmail.com>
2021-08-13 00:14:50 -0400

    Added Config Options

--- src/main/java/com/endremastered/endrem/EndRemastered.java
+++ src/main/java/com/endremastered/endrem/EndRemastered.java
@@ -1,17 +1,24 @@
 package com.endremastered.endrem;
 
+import com.endremastered.endrem.config.ConfigHandler;
 import com.endremastered.endrem.registry.BlockRegistry;
 import com.endremastered.endrem.registry.ItemRegistry;
 import com.endremastered.endrem.util.LootInjection;
 import com.endremastered.endrem.world.gen.OreSpawnHandler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tag.*;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class EndRemastered implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("End Remastered Logger");
 	public static final String MOD_ID = "endrem";
 	public static final ItemGroup ENDREM_TAB = FabricItemGroupBuilder.build(
 			createIdentifier("endrem_tab"),
@@ -27,5 +34,6 @@ public class EndRemastered implements ModInitializer {
 		BlockRegistry.init();
 		LootInjection.init();
 		OreSpawnHandler.Init();
+		ConfigHandler.load();
 	}
 }
